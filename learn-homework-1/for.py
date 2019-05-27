05ae78550b1a532520c1b68723d53fb93c282775
Vladimir Goryachev <vladimir.i.goryachev@gmail.com>
2019-05-27 14:23:26 +0400

    Домашнее по for

--- learn-homework-1/for.py
+++ learn-homework-1/for.py
@@ -11,11 +11,22 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    data_storage = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2, 3, 5]},
+                    {'school_class': '4b', 'scores': [3, 2, 2, 5, 2, 5]},
+                    {'school_class': '5a', 'scores': [3, 4, 5, 5, 2, 2]},
+                    {'school_class': '4b', 'scores': [5, 4, 2, 5, 2]},
+                    {'school_class': '6a', 'scores': [4, 4, 4, 3, 5, 2, 2]},
+                    {'school_class': '6b', 'scores': [3, 3, 5, 5]}]
+
+    classes_grade = {item['school_class']:(sum(item['scores'])/len(item['scores'])) for item in data_storage}
+    school_grade = sum(classes_grade.values()) / len(classes_grade.values())
+    
+    print("School summary grade is " + '{:.2f}'.format(school_grade))
+    print("Class summary grade are:")
+    for name_class, grade_class in classes_grade.items():
+        print("{}: {:.2f}".format(name_class,grade_class))
+
+    
     
 if __name__ == "__main__":
     main()
